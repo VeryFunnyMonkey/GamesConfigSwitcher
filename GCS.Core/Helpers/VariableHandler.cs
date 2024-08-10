da3667d6593599f311c0891dd56878883b92b22b
VeryFunnyMonkey <VeryFunnyMonkey@users.noreply.github.com>
2024-08-10 18:20:58 +0800

    corrected variableHandler to read the text once, and write back once, after all changes have been made. Also made it read and write in unicode encoding.

--- GCS.Core/Helpers/VariableHandler.cs
+++ GCS.Core/Helpers/VariableHandler.cs
@@ -1,21 +1,24 @@
+using System.Text;
+
 namespace GCS.Core
 {
     public static class VariableHandler
     {
         public static void useVariable(string profile, Dictionary<string, string> Variables)
         {
+            var fileContents = File.ReadAllText(profile, Encoding.Unicode);
+
             foreach (var kvp in Variables)
             {
                 var formattedVar = $"${{{kvp.Key}}}";
 
-                var fileContents = File.ReadAllText(profile);
-
-                if(fileContents.Contains(formattedVar))
+                if (fileContents.Contains(formattedVar))
                 {
                     fileContents = fileContents.Replace(formattedVar, kvp.Value);
-                    File.WriteAllText(profile, fileContents);
                 }
             }
+
+            File.WriteAllText(profile, fileContents, Encoding.Unicode);
         }
-    } 
+    }
 }
