5f149f04a3d0fda4cc576a59f7b9b71faf35ed4f
VeryFunnyMonkey <VeryFunnyMonkey@users.noreply.github.com>
2024-09-29 11:05:14 +0800

    added Serilog file logging, can be configured in appsettings.json

--- GCS.CLI/Program.cs
+++ GCS.CLI/Program.cs
@@ -1,13 +1,45 @@
 using Cocona;
 using GCS.CLI;
 using GCS.Core;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 var builder = CoconaApp.CreateBuilder();
 
+// Path to the configuration file
+var configFilePath = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+
+// Create the configuration file with default values if it doesn't exist
+if (!File.Exists(configFilePath))
+{
+    CreateDefaultConfigFile(configFilePath);
+}
+
+// Build Configuration
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+// Read logging settings from appsettings.json
+var loggingEnabled = configuration.GetValue<bool>("Logging:Enabled");
+var minimumLevel = configuration.GetValue<Serilog.Events.LogEventLevel>("Logging:minimumLevel");
+var filePath = configuration.GetValue<string>("Logging:FilePath");
+
 builder.Logging.ClearProviders();
-// TODO: Add file logging
+
+if (loggingEnabled)
+{
+    // Configure Serilog
+    Log.Logger = new LoggerConfiguration()
+        .MinimumLevel.Is(minimumLevel)
+        .WriteTo.File(filePath, rollingInterval: RollingInterval.Day)
+        .CreateLogger();
+    builder.Logging.AddSerilog(Log.Logger);
+}
+
 builder.Services.AddSingleton<ILoggingHandler, ConsoleLogger>();
 
 // Register IGameDataManager as a singleton service
@@ -45,3 +77,16 @@ app.AddCommands<UseCommand>();
 app.AddCommands<UseAllCommand>();
 
 app.Run();
+
+void CreateDefaultConfigFile(string path)
+{
+    var defaultConfig = @"
+{
+  ""Logging"": {
+    ""Enabled"": true,
+    ""MinimumLevel"": ""Information"",
+    ""FilePath"": ""logs/log-.log""
+  }
+}";
+    File.WriteAllText(path, defaultConfig);
+}
