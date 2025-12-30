7566951397c0b6e044e4b7693951dd9655a621d2
VeryFunnyMonkey <VeryFunnyMonkey@users.noreply.github.com>
2025-12-30 15:29:29 +0000

    deploy: 32731e7a3a25beeebeac82a6724f009f792a3253

--- search/all_9.js
+++ search/all_9.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['main_0',['main',['../main_8c.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main.c']]],
   ['main_2ec_1',['main.c',['../main_8c.html',1,'']]],
-  ['max_5fline_2',['MAX_LINE',['../gcs_8h.html#a842ed03f27719bc87666bfd1f75415b8',1,'gcs.h']]]
+  ['max_5fbuf_2',['MAX_BUF',['../gcs_8h.html#a2491303d0fe260aaba8f46717cda66cc',1,'gcs.h']]],
+  ['max_5fline_3',['MAX_LINE',['../gcs_8h.html#a842ed03f27719bc87666bfd1f75415b8',1,'gcs.h']]]
 ];
--- search/defines_3.js
+++ search/defines_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['max_5fline_0',['MAX_LINE',['../gcs_8h.html#a842ed03f27719bc87666bfd1f75415b8',1,'gcs.h']]]
+  ['max_5fbuf_0',['MAX_BUF',['../gcs_8h.html#a2491303d0fe260aaba8f46717cda66cc',1,'gcs.h']]],
+  ['max_5fline_1',['MAX_LINE',['../gcs_8h.html#a842ed03f27719bc87666bfd1f75415b8',1,'gcs.h']]]
 ];
