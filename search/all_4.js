14d51a0a7452c1fd65e58976c754a50a4ff8bb2e
VeryFunnyMonkey <VeryFunnyMonkey@users.noreply.github.com>
2025-12-30 15:46:30 +0000

    deploy: cda15437dfcb1018fd9485ab61eaa6a0f824ffe5

--- search/all_4.js
+++ search/all_4.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['delete_5fprofile_0',['delete_profile',['../config_8c.html#a73d3c9805e25c7c7dffcb44b9c22fce4',1,'delete_profile(const char *game, const char *prof):&#160;config.c'],['../gcs_8h.html#a73d3c9805e25c7c7dffcb44b9c22fce4',1,'delete_profile(const char *game, const char *prof):&#160;config.c']]],
   ['deleting_1',['Listing &amp; Deleting',['../index.html#autotoc_md12',1,'']]],
-  ['dst_2',['dst',['../structFilePair.html#a9e2adc6d7c005a2d20aca2eb269b8865',1,'FilePair']]]
+  ['documentation_2',['Documentation',['../index.html#autotoc_md16',1,'']]],
+  ['dst_3',['dst',['../structFilePair.html#a9e2adc6d7c005a2d20aca2eb269b8865',1,'FilePair']]]
 ];
